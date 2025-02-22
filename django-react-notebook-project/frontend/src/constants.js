72955a4b399329cacdc7c3dad6de181e81e24ee8
liu-jinxii <ryanlucan2024@outlook.com>
2025-02-22 16:32:33 +0800

    successfully lauch the project

--- django-react-notebook-project/frontend/src/constants.js
+++ django-react-notebook-project/frontend/src/constants.js
@@ -1,2 +1,2 @@
 export const ACCESS_TOKEN = "access";
-export const REFRESH_TOKEN = "refresh"
+export const REFRESH_TOKEN = "refresh"; // this constant here is the key to access the access/refresh token I stored in the broswer
