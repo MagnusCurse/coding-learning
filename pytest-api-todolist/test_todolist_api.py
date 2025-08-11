d16b02de5236f745d3e4cd484f50afce4bdfce13
liu-jinxii <ryanlucan2024@outlook.com>
2025-08-11 16:30:31 +0800

    API test to create todo list task

---
+++ pytest-api-todolist/test_todolist_api.py
@@ -0,0 +1,39 @@
+import requests
+
+ENDPOINT = "https://todo.pixegami.io/"
+
+def test_can_call_endpoint():
+    response = requests.get(ENDPOINT)
+    assert response.status_code == 200
+
+
+def test_can_create_todo_task():
+    payload = {
+        "content": "test_content",
+        "user_id": "234534234234",
+        "is_done": False
+    }
+
+    create_task_response = requests.put(ENDPOINT + "/create-task", json=payload)
+    assert create_task_response.status_code == 200
+
+    create_task_data = create_task_response.json()
+    print(create_task_data)
+    print(type(create_task_data))
+
+    task_id = create_task_data.get('task').get('task_id')  # get the task_id from the create_task_data
+    get_task_response = requests.get(ENDPOINT + f"/get-task/{task_id}")  # get the task using the task_id
+
+    assert get_task_response.status_code == 200
+    get_task_data = get_task_response.json()
+    # assert the content, user_id, and is_done are the same as the payload
+    assert get_task_data.get('content') == payload.get('content')
+    assert get_task_data.get('user_id') == payload.get('user_id')
+    assert get_task_data.get('is_done') == payload.get('is_done')
+   
+
+
+
+
+
+
